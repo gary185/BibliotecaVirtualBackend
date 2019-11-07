7c8dfc7ed68d4e2e37fc6814cfbfeb5cc35ca96d
Gary Nuñez <gnunezc@unsa.edu.pe>
2019-11-07 01:58:54 -0500

    Installing dependencies

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//importing routes
+
+//settings
+
+
+//starting the server
+app.listen(4000, () => {
+	console.log('Server running on port 4000');
+})
