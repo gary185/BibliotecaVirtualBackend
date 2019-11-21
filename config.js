da63c1e59cd999f9336b428473a9179b2dab8cb1
Gary Nuñez <gnunezc@unsa.edu.pe>
2019-11-21 00:02:07 -0500

    Fixing database connection

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://ds137508.mlab.com:37508/heroku_9kvhs1mj',
+    db: process.env.MONGODB || 'mongodb://heroku_9kvhs1mj:k8tgtrrj832mkh80m6n67b7qhu@ds137508.mlab.com:37508/heroku_9kvhs1mj',
     SECRET_TOKEN: 'miclavetoken'
 }
