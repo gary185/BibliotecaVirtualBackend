0822f054614378623c511211b14f230657d1e89a
Gary Nuñez <gnunezc@unsa.edu.pe>
2019-11-20 23:30:52 -0500

    Fix2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://@ds137508.mlab.com:37508/easynotebd',
+    db: process.env.MONGODB || 'mongodb://ds137508.mlab.com:37508/easynotebd',
     SECRET_TOKEN: 'miclavetoken'
 }
